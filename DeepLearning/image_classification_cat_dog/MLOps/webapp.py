7db70724b8253af96c967fe109d9ef9a6f39ae2d
Suraj <hrishabhsuraj52@gmail.com>
2021-02-11 19:54:01 +0530

    Updated code

--- DeepLearning/image_classification_cat_dog/MLOps/webapp.py
+++ DeepLearning/image_classification_cat_dog/MLOps/webapp.py
@@ -12,21 +12,22 @@ import numpy as np
 import shutil
 #loading the model
 classification_model = tensorflow.keras.models.load_model('TrainedModel.h5')
-#classification function
-# def visualize(img):
-#   plt.imshow(img)
-#   plt.show()
-def classify(img):
-      x = img_to_array(img)
-      x = np.expand_dims(x, axis=0)
-      x = preprocess_input(x)
-      preds = classification_model.predict(x)
-    #print(float(preds[0][1]))
-      if float(preds[0][1]) > float(preds[0][0]):
-         out_class = "Cat"
-      elif float(preds[0][1]) < float(preds[0][0]):
-         out_class = "Dog"
-      return out_class
+#classification class
+class ClassifyImage:
+    #constructor
+    def __init__(self,image:np.ndarray):
+        self.image = image
+    #classifier method
+    def classify(self):
+          x = np.asarray(self.image.resize((96, 96)))[..., :3]
+          x = np.expand_dims(x, axis=0)
+          x = preprocess_input(x)
+          preds = classification_model.predict(x)
+          if float(preds[0][1]) > float(preds[0][0]):
+             out_class = "Cat"
+          elif float(preds[0][1]) < float(preds[0][0]):
+             out_class = "Dog"
+          return out_class
 
 #defining the object of FastAPI
 classifier_app = FastAPI()
@@ -36,27 +37,18 @@ classifier_app = FastAPI()
 def web_app():
     return {'Welcome to the Classifier app'}
 #recieving the image from form
-@classifier_app.get('/predict')
-async def predict_class(image: UploadFile = File(...)):
-     #supported extensions
-     ext = [".png",".jpg",".JPEG"]
-     if image.content_type.endswith(tuple(ext)) is False:
-        raise HTTPException(status_code=400, detail=f'File \'{image.filename}\' is not an JPEG,jpg,png image.')
-     try:
-        contents = await image.read()
-        image = Image.open(io.BytesIO(contents)).convert('RGB')
-        logging.info("image read")
-        image = image.resize((96,96))
-        logging.info("image resized to {0}".format(np.shape(image)))
-        label = classify(image)
-        logging.info("classification label", label)
-        return {"Predicted Label is {0}".format(label)}
-
-     except Exception as error:
-        logging.exception(error)
-        e = sys.exc_info()[1]
-        raise HTTPException(status_code=500, detail=str(e))
-
+@classifier_app.post('/predict')
+async def predict_class(file: UploadFile = File(...)):
+    extension = file.filename.split(".")[-1] in ("jpg", "jpeg", "png")
+    if not extension:
+        return "Only use jpg,jpeg or png image to evaluate."
+    contents = await file.read()
+    #reading the encoded image
+    img = Image.open(io.BytesIO(contents))
+    #creating an object of classifier
+    classifier = ClassifyImage(img)
+    label = classifier.classify()
+    return {"Predicted Label is {0}".format(label)}
 
 if __name__ == '__main__':
     uvicorn.run(classifier_app,'127.0.0.1')
